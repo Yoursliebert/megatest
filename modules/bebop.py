2f0d4378e02d7945328181640478be71289416e1
Yoursliebert <yoursliebert777@gmail.com>
2025-07-13 12:09:12 +0700

    Update bebop.py

--- modules/bebop.py
+++ modules/bebop.py
@@ -47,25 +47,27 @@ contract = w3.eth.contract(address=WETH_CONTRACT, abi=contract_abi)
 
 def get_random_amount():
     min_val = 0.00001
-    max_val = 0.00002
+    max_val = 0.00005
     amount = random.uniform(min_val, max_val)
-    amount = float(f"{amount:.4f}")
+    amount = float(f"{amount:.6f}")
     return w3.to_wei(amount, "ether")
 
-def get_random_delay():
-    return random.randint(1 * 60 * 1000, 3 * 60 * 1000)
+def get_random_delay(min_sec=10, max_sec=20):
+    return random.randint(min_sec, max_sec)
 
-def wrap_mon(amount):
+def wrap_eth(amount):
     try:
         print(Fore.BLUE + "🪫 Starting Bebop ⏩⏩⏩⏩")
         print(Fore.MAGENTA + f"🔄 Wrapping {w3.from_wei(amount, 'ether')} ETH to WETH")
         nonce = w3.eth.get_transaction_count(account.address)
+        gas_price = w3.eth.gas_price
         tx = contract.functions.deposit().build_transaction({
             'from': account.address,
             'value': amount,
-            'gas': 210000,
-            'nonce': nonce,
-            'gasPrice': w3.eth.gas_price
+            'gas': 200000,
+            'nonce': w3.eth.get_transaction_count(account.address),
+            'maxFeePerGas': gas_price + w3.to_wei(0.00012, 'gwei'),  # naikkan 3-5 GWEI
+            'maxPriorityFeePerGas': w3.to_wei(0.00015, 'gwei')
         })
         signed_tx = account.sign_transaction(tx)
         tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
@@ -76,15 +78,17 @@ def wrap_mon(amount):
     except Exception as e:
         print(Fore.RED + f"❌ Error while wrapping ETH to WETH: {str(e)}")
 
-def unwrap_mon(amount):
+def unwrap_eth(amount):
     try:
         print(Fore.MAGENTA + f"🔄 Unwrapping {w3.from_wei(amount, 'ether')} WETH to ETH")
         nonce = w3.eth.get_transaction_count(account.address)
+        gas_price = w3.eth.gas_price
         tx = contract.functions.withdraw(amount).build_transaction({
             'from': account.address,
-            'gas': 210000,
-            'nonce': nonce,
-            'gasPrice': w3.eth.gas_price
+            'gas': 200000,
+            'nonce': w3.eth.get_transaction_count(account.address),
+            'maxFeePerGas': gas_price + w3.to_wei(0.00012, 'gwei'),  # naikkan 3-5 GWEI
+            'maxPriorityFeePerGas': w3.to_wei(0.00015, 'gwei')
         })
         signed_tx = account.sign_transaction(tx)
         tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
@@ -93,19 +97,19 @@ def unwrap_mon(amount):
         # Mengganti waitForTransactionReceipt dengan wait_for_transaction_receipt
         w3.eth.wait_for_transaction_receipt(tx_hash)
     except Exception as e:
-        print(Fore.RED + f"❌ Error while unwrapping WMON to MON: {str(e)}")
+        print(Fore.RED + f"❌ Error while unwrapping WETH to ETH: {str(e)}")
 
 def run_swap_cycle(cycles=1):
     try:
         for i in range(cycles):
             random_amount = get_random_amount()
             random_delay = get_random_delay()
-            wrap_mon(random_amount)
-            unwrap_mon(random_amount)
-            if i < cycles - 1:
-                wait_minutes = random_delay / 1000 / 60
-                print(Fore.LIGHTBLACK_EX + f"⏳ Waiting for {wait_minutes} minutes")
-                time.sleep(random_delay / 1000)
+            wrap_eth(random_amount)
+            time.sleep(5)
+            unwrap_eth(random_amount)
+            if i < cycles - 1:           
+                print(Fore.LIGHTBLACK_EX + f"⏳ Waiting for {random_delay} seconds")
+                time.sleep(random_delay)
         print(Fore.GREEN + "✅ Finished")
     except Exception as e:
         print(Fore.RED + f"❌ Error during swap cycle: {str(e)}")
